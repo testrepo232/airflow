0022fc89237b2aeec5e2af3b81bf017bcd8c47e1
testrepo232 <askoventures17@gmail.com>
2025-03-04 22:31:06 +0100

    Create spark_dag.py

---
+++ dag/spark_dag.py
@@ -0,0 +1,27 @@
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'Airflow',
+}
+
+with DAG(
+    dag_id='example_spark_operator',
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example'],
+) as dag:
+    # [START howto_operator_spark_submit]
+    
+    python_submit_job = SparkSubmitOperator(
+        application="/workspace/example-airflow-and-spark/spark-3.1.1-bin-hadoop2.7/examples/src/main/python/pi.py", task_id="python_job"
+    )
+    
+    scala_submit_job = SparkSubmitOperator(
+        application="/workspace/example-airflow-and-spark/airflow-spark-assembly-0.1.0-SNAPSHOT.jar", task_id="scala_job"
+    )
+
+    python_submit_job >> scala_submit_job
+    
