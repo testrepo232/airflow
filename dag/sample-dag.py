35f01c5ddffceb32ef5bfd15b510269ec0c204b7
testrepo232 <askoventures17@gmail.com>
2025-03-06 23:21:27 +0100

    Update sample-dag.py

--- dag/sample-dag.py
+++ dag/sample-dag.py
@@ -1,21 +1,18 @@
 from airflow import DAG
 from datetime import timedelta, datetime
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import 
-SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import 
-SparkKubernetesSensor
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.models import Variable
 from kubernetes.client import models as k8s
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
-default_args={
+default_args = {
    'depends_on_past': False,
-   'email': ['[email protected]'],
+   'email': ['[email protected]'],
    'email_on_failure': False,
    'email_on_retry': False,
    'retries': 1,
    'retry_delay': timedelta(minutes=5)
 }
+
 with DAG(
    'my-second-dag',
    default_args=default_args,
@@ -32,7 +29,7 @@ with DAG(
        retries=3,
        application_file="new-spark-pi.yaml",
        namespace="spark-jobs",
-       kubernetes_conn_id="myk8s",
+       kubernetes_conn_id="spark-k8s",  # Use your connection ID
        api_group="sparkoperator.k8s.io",
        api_version="v1beta2",
        do_xcom_push=True,
