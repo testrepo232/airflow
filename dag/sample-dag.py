d1aee300b88aed76e9c72fa16f2f8033c023dfdc
testrepo232 <askoventures17@gmail.com>
2025-03-07 00:11:05 +0100

    Update sample-dag.py

--- dag/sample-dag.py
+++ dag/sample-dag.py
@@ -1,37 +1,28 @@
 from airflow import DAG
-from datetime import timedelta, datetime
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.models import Variable
-from kubernetes.client import models as k8s
+from datetime import datetime, timedelta
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 default_args = {
-   'depends_on_past': False,
-   'email': ['[email protected]'],
-   'email_on_failure': False,
-   'email_on_retry': False,
-   'retries': 1,
-   'retry_delay': timedelta(minutes=5)
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
 with DAG(
-   'my-second-dag',
-   default_args=default_args,
-   description='simple dag',
-   schedule_interval=timedelta(days=1),
-   start_date=datetime(2022, 11, 17),
-   catchup=False,
-   tags=['example']
+    'spark-pi-dag',
+    default_args=default_args,
+    description='Pi Calculation with Spark',
+    schedule_interval=None,  # Trigger manually
+    start_date=datetime(2022, 11, 17),
+    catchup=False,
 ) as dag:
-   t1 = SparkKubernetesOperator(
-       task_id='n-spark-pi',
-       trigger_rule="all_success",
-       depends_on_past=False,
-       retries=3,
-       application_file="new-spark-pi.yaml",
-       namespace="spark-jobs",
-       kubernetes_conn_id="spark-k8s",  # Use your connection ID
-       api_group="sparkoperator.k8s.io",
-       api_version="v1beta2",
-       do_xcom_push=True,
-       dag=dag
-   )
+    spark_task = SparkSubmitOperator(
+        task_id='spark-pi-job',
+        conn_id='spark_default',  # Your Spark connection ID
+        application='/home/deployer/projects/spark-jobs/sparkk_job.py',  # Full path to your Python job
+        application_args=['1000000'],  # Optional: Number of samples for the Pi calculation
+        name='spark-pi-job',
+        dag=dag
+    )
