34b863127cda350902ec585cee66b28a3ea939ab
Varvara Yasko <askovarvara@gmail.com>
2025-03-07 22:06:13 +0000

    add my_simple_spark_dag

---
+++ dag/my_simple_spark_dag.py
@@ -0,0 +1,27 @@
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'Airflow',
+}
+
+with DAG(
+    dag_id='my_simple_spark_dag',
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example','my_dag'],
+) as dag:
+    # [START howto_operator_spark_submit]
+    
+    python_submit_job = SparkSubmitOperator(
+        application="/opt/my_simple_spark_dag.py", task_id="python_job"
+    )
+    
+#    scala_submit_job = SparkSubmitOperator(
+#        application="/workspace/example-airflow-and-spark/airflow-spark-assembly-0.1.0-SNAPSHOT.jar", task_id="scala_job"
+#    )
+#
+#    python_submit_job >> scala_submit_job
+#    
---
+++ my_simple_spark_dag.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# PDU & Equinix monitoring script
+### 20250303 1.0 Create basic idea
+
+print(f"I hate dumb spark!")
+#EOF
+
