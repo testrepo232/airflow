04f67b872150557cfb63a438b285b83c50702803
Varvara Yasko <askovarvara@gmail.com>
2025-03-07 22:14:25 +0000

    fix my_simple_spark_dag

--- dag/my_simple_spark_dag.py
+++ dag/my_simple_spark_dag.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from airflow.models import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.utils.dates import days_ago
@@ -14,14 +16,15 @@ with DAG(
     tags=['example','my_dag'],
 ) as dag:
     # [START howto_operator_spark_submit]
-    
+
     python_submit_job = SparkSubmitOperator(
-        application="/opt/my_simple_spark_dag.py", task_id="python_job"
+        application="/opt/airflow/dags/repo/my_simple_spark_dag.py", task_id="python_job"
     )
-    
+
 #    scala_submit_job = SparkSubmitOperator(
 #        application="/workspace/example-airflow-and-spark/airflow-spark-assembly-0.1.0-SNAPSHOT.jar", task_id="scala_job"
 #    )
 #
 #    python_submit_job >> scala_submit_job
-#    
+#
+#EOF
